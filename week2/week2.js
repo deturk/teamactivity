1d6c30c857e322957fa55cefa3bf610d3dc5aedc
atokb <ato15001@byui.edu>
2019-09-28 15:06:57 -0600

    2 done

--- week2/week2.js
+++ week2/week2.js
@@ -1,4 +1,23 @@
 function add() {
-   var result = document.getElementById('userInput').value;
-   document.getElementById('output').innerHTML = result;
+    var result = document.getElementById('userInput').value;
+    document.getElementById('output').innerHTML = result;
+}
+
+
+function provideNum() {
+    var getNumber = document.getElementById('numInput').value;
+    var giveNumber = document.getElementById("numOutput");
+
+    var intNum = parseInt(getNumber);
+    if (intNum) {
+        giveNumber.innerHTML = "total:" + sum(intNum);
+    }
+}
+
+function sum(number) {
+    var total = 0;
+    for (i = 0; i <= number; i++) {
+        total += i;
+    }
+    return total;
 }
