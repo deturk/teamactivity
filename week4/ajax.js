da6b3a5e33e8ba7f54420f61c6e699f63d343bda
smainoo <mai15005@byui.edu>
2019-10-31 09:33:42 -0600

    ajax

--- week4/ajax.js
+++ week4/ajax.js
@@ -17,4 +17,32 @@ function longProcess(){
 }
 longProcess().then(result=>{
     console.log('total', 10 + result);
-}).catch;
+}).catch(err=>{
+    console.log(err);
+});
+
+const baseUrl = 'https://pokeapi.co/api/v2/';
+
+function getJson(url){
+    return fetch(baseUrl + url).then(res=>{
+        if(res.ok){
+            return res.json();
+        }else{
+            console.log('error!');
+            throw new Error('bad response');
+            
+        }
+    }).catch(err =>{
+        console.log('getJson', err);
+    });
+}
+const myList = document.getElementById('list');
+
+getJson('type/3').then(data =>{
+    console.log(data);
+    const myNewArray = data.pokemon.map(item =>{
+        return `<li>${item.pokemon.name}<li>`;
+    }).join('');
+    // console.log(myNewArray);
+    myList.innerHTML = myNewArray;
+});
