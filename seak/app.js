0cb140ccc2b4864e55517f78aee4684d35e02249
smainoo <mai15005@byui.edu>
2019-12-04 14:43:27 -0700

    data pulling worked

--- seak/app.js
+++ seak/app.js
@@ -3,24 +3,22 @@ import {auth,dbase} from './firebase.js';
 
 //let docs = null ;
 
- dbase.collection('users').get().then((snapshot) => {
-    //console.log(snapshot.docs);
-    snapshot.docs.forEach(doc => {
-        let docData = doc.data();
-        //console.log(docData);
-        if(docData.email == 'cat@gmail.com'){
-            renderUserInfo(doc);
-            //document.getElementById("li").style.display = "none";
+//create a function to get user info
+let userData = null;
+function getUserInfo(userId){
+    dbase.collection('users').where("email", "==", userId)
+    .get().then((snapshot) => {
+        console.log(snapshot.docs[0].id);
+        //console.log(snapshot);
+            
+            userData = snapshot.docs[0].data();
+            
+        });
+}
+ 
+    
+    
 
-        }
-        
-        //return docData;
-        
-    });
-    
-    
-});
-//console.log(doc);
 
 //console.log(docs.email);
 
@@ -29,24 +27,27 @@ import {auth,dbase} from './firebase.js';
 auth.onAuthStateChanged(user => {
     if (user) {
         console.log(user.email, 'logged in');
+          getUserInfo(user.email);
+
+          
     } else {
         console.log('user logged out');
     }
 });
 
 //get document from the database and render it in the frontend
-function renderUserInfo(doc){
+function renderUserInfo(){
     let item = document.createElement('li');
     let firstname = document.createElement('div');
     let lastname = document.createElement('div');
     let email = document.createElement('div');
     item.classList.add('light');
 
-
-    item.setAttribute('data-id', doc.id);
-    firstname.textContent = doc.data().firstname;
-    lastname.textContent = doc.data().lastname;
-    email.textContent = doc.data().email;
+    console.log(userData);
+    item.setAttribute('data-id', userData.id);
+    firstname.textContent = userData.firstname;
+    lastname.textContent = userData.lastname;
+    email.textContent = userData.email;
 
     item.appendChild(firstname);
     item.appendChild(lastname);
@@ -57,17 +58,14 @@ function renderUserInfo(doc){
  
 }
 
+
 //listen to click event on user details.
 const userAccount = document.querySelector('#accountDetails');
 
 userAccount.addEventListener('touchend', (e) =>{
     e.preventDefault();
-    //renderUserInfo(doc);
-    document.getElementById('accountDetails').style.display = "block";
+    renderUserInfo();
     
-    
-    
-
 });
 
 
