775bc2bf334620a7520b204150c53b101bc55d44
smainoo <mai15005@byui.edu>
2019-12-05 01:40:33 -0700

    user details working

--- seak/app.js
+++ seak/app.js
@@ -10,6 +10,7 @@ function getUserInfo(userId){
         //console.log(snapshot.docs[0].id);
         //console.log(snapshot);
         userData = snapshot.docs[0].data();
+        renderUserInfo();
             
         });
 }
@@ -27,36 +28,47 @@ auth.onAuthStateChanged(user => {
 });
 
 //get document from the database and render it in the frontend
+
+let item = document.createElement('ul');
 function renderUserInfo(){
-    let item = document.createElement('li');
+    
     let firstname = document.createElement('div');
     let lastname = document.createElement('div');
     let email = document.createElement('div');
     item.classList.add('light');
 
-    console.log(userData);
+    //console.log(userData);
     item.setAttribute('data-id', userData.id);
     firstname.textContent = userData.firstname;
     lastname.textContent = userData.lastname;
     email.textContent = userData.email;
 
+
     item.appendChild(firstname);
     item.appendChild(lastname);
     item.appendChild(email);
-
     userAccount.appendChild(item);
+    item.style.display = "none";
+
     
- 
 }
 
+//toggle the user account content
+// function hideUserDetails(){
+//    userAccount.removeChild(item);
+// }
 
 //listen to click event on user details.
 const userAccount = document.querySelector('#accountDetails');
 
 userAccount.addEventListener('touchend', (e) =>{
     e.preventDefault();
-    renderUserInfo();
-    
+
+    if(item.style.display === "none"){
+        item.style.display = "block";
+    }else {item.style.display = "none";
+    }
+    //renderUserInfo();
 });
 
 
