87022288c5113bd0e56deb0c94a75b6473090a1f
atokb <ato15001@byui.edu>
2019-12-12 10:58:20 -0700

    fixed stuff

--- seak/app.js
+++ seak/app.js
@@ -29,9 +29,7 @@ dbase.collection('customers')
             console.log(doc.data());
         });
     });
-//console.log(docs.email);
 
-//const userInfo = document.querySelector('#userinfo');
 
 auth.onAuthStateChanged(user => {
     if (user) {
@@ -43,6 +41,7 @@ auth.onAuthStateChanged(user => {
         console.log('user logged out');
     }
 });
+
 
 //get document from the database and render it in the frontend
 let item = document.createElement('ul');
@@ -70,64 +69,6 @@ function renderUserInfo() {
 }
 item.style.display == 'none';
 
-// hide and show customer divs
-
-function customerDivs() {
-    let divs = document.createElement('ul');
-    let addOption = document.createElement('div');
-    let viewOption = document.createElement('div');
-
-    addOption.textContent = 'Add Customer';
-    viewOption.textContent = 'View Customers';
-
-    divs.appendChild(addOption);
-    divs.appendChild(viewOption);
-
-    divs.classList.add('divOptions');
-
-
-    customerInfo.appendChild(divs);
-    console.log(divs);
-
-}
-
-// function to display customer options
-let options = document.createElement('ul');
-let addOption = document.createElement('div');
-let viewOption = document.createElement('div');
-
-function addCustomerOptions(doc) {
-
-    addOption.textContent = 'Add';
-    viewOption.textContent = 'View';
-
-    options.classList.add('divOptions');
-    addOption.classList.add('addCustomer');
-    viewOption.classList.add('viewCustomer');
-
-    options.setAttribute('data-id', doc.id);
-    options.appendChild(addOption);
-    options.appendChild(viewOption);
-
-    customerInfo.appendChild(options);
-}
-
-
-addOption.textContent = 'Add';
-viewOption.textContent = 'View';
-
-options.classList.add('divOptions');
-addOption.classList.add('addCustomer');
-viewOption.classList.add('viewCustomer');
-
-//options.setAttribute('data-id', doc.id);
-options.appendChild(addOption);
-options.appendChild(viewOption);
-
-customerInfo.appendChild(options);
-}
-options.style.display == 'none';
-
 //listen to click event on user details.
 userAccount.addEventListener('touchend', (e) => {
     e.preventDefault();
@@ -140,65 +81,100 @@ userAccount.addEventListener('touchend', (e) => {
     renderUserInfo();
 });
 
-let customerList = document.createElement('ul');
-async function viewCustomers(doc) {
 
-    let firstname = document.createElement('div');
-    let lastname = document.createElement('div');
-    let street = document.createElement('div');
-    let city = document.createElement('div');
-    let state = document.createElement('div');
-    let phone = document.createElement('div');
-    customerList.classList.add('customer-list');
+function getCustomerData() {
+    dbase.collection('customers').get()
+        .then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                addCustomerOptions(doc);
+            });
 
-    customerList.setAttribute('data-id', doc.id);
-    firstname.textContent = `First Name : ${doc.data().firstname}`;
-    lastname.textContent = `Last Name: ${doc.data().lastname}`;
-    street.textContent = `Street Address: ${doc.data().streetaddress}`;
-    city.textContent = `City: ${doc.data().city}`;
-    state.textContent = `State: ${doc.data().state}`;
-    phone.textContent = `Phone: ${doc.data().phone}`;
-
-
-    customerList.appendChild(firstname);
-    customerList.appendChild(lastname);
-    customerList.appendChild(street);
-    customerList.appendChild(city);
-    customerList.appendChild(state);
-    customerList.appendChild(phone);
-
-
-    
-    customerInfo.appendChild(customerList);
+        });
 }
 
 //call function to get customer info on event
-const customerInfo = document.querySelector('#customer');
+const customerInfo = document.querySelector('.addCustomer');
 
-customerInfo.addEventListener('click', (e) => {
+customerInfo.addEventListener('touchend', (e) => {
     e.preventDefault();
     getCustomerData();
     addForm();
 });
 
-<<<<<<< HEAD
 function addForm() {
-    let addForm = document.querySelector('#add-form');
-    addForm.addEventListener('click', (e) => {
-=======
- function addForm(){
-    let addForm = document.querySelector('.addCustomer');
-    addForm.addEventListener('click', (e)=>{
->>>>>>> parent of 3eeb71b... Revert "everything is working now. the form is hidden behind the menu. someone should fix it"
-        document.getElementById('add-form').style.display = "block";
-
-    });
-
+    document.body.innerHTML = `
+    <nav role="navigation">
+            <div id="menuToggle">
+                <!--
+                            A fake / hidden checkbox is used as click reciever,
+                            so you can use the :checked selector on it.
+                            -->
+                <input type="checkbox" />
+                <!--
+                            Some spans to act as a hamburger.
+                            
+                            They are acting like a real hamburger,
+                            not that McDonalds stuff.
+                            -->
+                <span></span>
+                <span></span>
+                <span></span>
+                <!--
+                            Too bad the menu has to be inside of the button
+                            -->
+                            <ul id="menu">
+                            <a href="main.html">
+                                <li>Home</li>
+                            </a>
+                            <a href="#">
+                                <li>Account Details</li>
+                            </a>
+                            <a href="contactus.html">
+                                <li>Contact Us</li>
+                            </a>
+                            <a href="#">
+                                <li id="logout">Log Out</li>
+                            </a>
+                        </ul>
+            </div>
+        </nav>
+        <div id="contactForm" class="contactForm">
+        <div id="formHeader" class="formHeader">
+    <h2 id="message">Add New Customer</h2>
+    </div>
+<div id="formBody" class="formBody">
+    <form inctype=text/plain>
+    <div class="inputContainer">
+      <input type="text" id="firstname" name="firstname" placeholder="First Name">
+    </div>
+    <div class="inputContainer">
+      <input type="text"  id="lastname" name="lastname" placeholder="Last Name">
+    </div>
+    <div class="inputContainer">    
+      <input type="text"  id="email" name="email" placeholder="Email">
+    </div>
+    <div class="inputContainer">
+      <input type="text"  id="street" name="street" placeholder="Street Address">
+    </div>
+    <div class="inputContainer">
+      <input type="text"  id="city" name="city" placeholder="City">
+    </div>
+    <div class="inputContainer">
+      <input type="text"  id="state" name="state" placeholder="State">
+    </div>
+    <div class="inputContainer">
+       <input type="text" id="phone" name="phone" placeholder="(222)-222-2222">
+    </div>
+      <input type="submit" class="submitBtn" id="addNewCustomer" value="Add Customer">
+      <br><br>
+    </form>
+    </div>
+    </div>
+    `;
 
 }
 
 //save form values to database
-<<<<<<< HEAD
 function addCustomerForm() {
     const addNewCustomer = document.querySelector('#addNewCustomer');
     addNewCustomer.addEventListener('touchend', (e) => {
@@ -220,43 +196,15 @@ function addCustomerForm() {
             state: state,
             phone: phone
         });
-=======
-const addNewCustomer = document.querySelector('#addNewCustomer');
-   addNewCustomer.addEventListener('click', (e) => {
-    e.preventDefault();
-    const firstname = document.getElementById('firstname').value;
-    const lastname = document.getElementById('lastname').value;
-    const email = document.getElementById('email').value;
-    const street = document.getElementById('street').value;
-    const city = document.getElementById('city').value;
-    const state = document.getElementById('state').value;
-    const phone = document.getElementById('phone').value;
-    // grab data from  the form and save to databse
-    dbase.collection('customers').add({
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        street: street,
-        city: city,
-        state: state,
-        phone: phone
->>>>>>> parent of 3eeb71b... Revert "everything is working now. the form is hidden behind the menu. someone should fix it"
     });
 }
 
 
 //get customer information
-// const customerBtn = document.querySelector('.view');
-
-// customerBtn.addEventListener('touchend', (e) => {
-//     e.preventDefault();
-//     viewCustomers();
-// });
-
 
 let customerList = document.createElement('ul');
 //function to view customers
-async function viewCustomers(doc) {
+function viewCustomers(doc) {
 
     let firstname = document.createElement('div');
     let lastname = document.createElement('div');
@@ -273,7 +221,6 @@ async function viewCustomers(doc) {
     city.textContent = doc.data().city;
     state.textContent = doc.data().state;
     phone.textContent = doc.data().phone;
-
 
     customerList.appendChild(firstname);
     customerList.appendChild(lastname);
